d0f23a4dc4563b11963a6d1f9b9e1f13b5fd0934
João Neto Pereira Gregorio <joaonetopereira0@gmail.com>
2026-04-17 15:48:32 -0300

    corrigindo

--- Front/script.js
+++ Front/script.js
@@ -1,6 +1,7 @@
-const API_USUARIO = "http://192.168.1.208:30082"
-const API_DISCIPLINA = "http://192.168.1.208:30081"
-const API_MATRICULA = "http://192.168.1.208:30083"
+const API_USUARIO = "/api/usuario"
+const API_DISCIPLINA = "/api/disciplina"
+const API_MATRICULA = "/api/matricula"
+
 
 function mostrarCadastrarUsuario(){
 
