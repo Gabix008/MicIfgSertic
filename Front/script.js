abcfbf969e9b95a82861f5ff9ec67b183f28fbeb
João Neto Pereira Gregorio <joaonetopereira0@gmail.com>
2026-04-22 12:31:40 -0300

    teste

--- Front/script.js
+++ Front/script.js
@@ -1,6 +1,6 @@
-const API_USUARIO = "http://192.168.1.208:30081"
-const API_DISCIPLINA = "http://192.168.1.208:30082"
-const API_MATRICULA = "http://192.168.1.208:30083"
+const API_USUARIO = ""
+const API_DISCIPLINA = ""
+const API_MATRICULA = ""
 
 
 function mostrarCadastrarUsuario(){
