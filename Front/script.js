e5a90c68265005a0ccd411f26c7de9a466205035
João Neto Pereira Gregorio <joaonetopereira0@gmail.com>
2026-04-22 10:40:41 -0300

    k3s funcionando

--- Front/script.js
+++ Front/script.js
@@ -1,6 +1,6 @@
-const API_USUARIO = ""
-const API_DISCIPLINA = ""
-const API_MATRICULA = ""
+const API_USUARIO = "http://localhost:30081"
+const API_DISCIPLINA = "http://localhost:30082"
+const API_MATRICULA = "http://localhost:30083"
 
 
 function mostrarCadastrarUsuario(){
