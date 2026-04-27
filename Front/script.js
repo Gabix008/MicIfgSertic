75d3e1494ffe409a74eb92e950064723f1f0fc19
João Neto Pereira Gregorio <joaonetopereira0@gmail.com>
2026-04-27 14:01:13 -0300

    alterando chamada

--- Front/script.js
+++ Front/script.js
@@ -1,6 +1,6 @@
-const API_USUARIO = "http://localhost:8082"
-const API_DISCIPLINA = "http://localhost:8082"
-const API_MATRICULA = "http://localhost:8082"
+const API_USUARIO = "http://localhost:8080"
+const API_DISCIPLINA = "http://localhost:8080"
+const API_MATRICULA = "http://localhost:8080"
 
 
 function mostrarCadastrarUsuario(){
