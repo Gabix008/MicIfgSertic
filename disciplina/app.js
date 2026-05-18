f82a4a3a0ad36ff1f34b1dcceddbf7a132034038
SilvaOscar <silva.oscar@estudantes.ifg.edu.br>
2026-05-18 20:01:18 -0300

    Atualização cors

--- disciplina/app.js
+++ disciplina/app.js
@@ -1,9 +1,7 @@
 const express = require("express")
 const fs = require("fs")
-const cors = require("cors")
 const axios = require("axios")
 const app = express()
-app.use(cors())
 app.use(express.json())
 
 const path = "./disciplina.json"
